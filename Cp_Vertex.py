fc10955d17d7f248adf06a223765662013ebce19
Sylvain Page(s.page@hpo-samat.com) <user11@hpo-samat.fr>
2017-02-24 13:41:19 +0100

    update
    Opérationnelle sous Vertex

--- Cp_Vertex.py
+++ Cp_Vertex.py
@@ -4,16 +4,23 @@ Created on Wed Nov 30 10:37:08 2016
 
 @author: user11
 """
-import os
+import os, sys
 import shutil
+import subprocess
 import glob
 import xlwings as xw
 import numpy as np
 import pandas as pd
+
+
+path=os.getcwd()
+sys.path.append(r"G:\01-SUIVI PROCESSUS\GMM\Micro-Vu Vertex\Python")
 from csv2xls import to_xls, file2df
 
 
-path = r'G:\01-SUIVI PROCESSUS\GMM\Micro-Vu Vertex\Hirtenberger\27214161'
+path = r'G:\01-SUIVI PROCESSUS\GMM\Micro-Vu Vertex\Schrader\43418-820'
+cde = path + '\\iscmd.exe'
+file = path + '/43418-820.iwp'
 listp = ['P'+str(x) for x in range (1,51)]
 
 
@@ -29,11 +36,15 @@ except:
 
 for P in listp:
     # lancer le programme vertex avec une ligne de commande
+    subprocess.call("iscmd.exe /run" + path +"/43418-820.iwp")
+    # Récupérer le fichier Csv
     fcsv = glob.glob(path+"/CSV Data/*.txt")[-1]  # File containing CSV
+    # Transformation en Dataframe des données csv
     data = file2df(fcsv)
+    # Déplacer le fichier dans un répertoire Cp
     shutil.move(fcsv,path+"/Cp")
     df = data.copy()
-    df['P'] = P #ajout du N° pièce
+    df['P'] = P #ajout du N° pièce (Px)
     # ajouter les résultats à la liste
     listdf.append(df)
 df = pd.concat(listdf)
